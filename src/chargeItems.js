83091bbb49acc8467ac876eb7b630829ee2b7e65
santiago <santiagoberrio0@gmail.com>
2021-08-03 16:18:33 -0500

    Webpack bundle, delete button

---
+++ src/chargeItems.js
@@ -0,0 +1,85 @@
+import {chargeId } from './idManipulation';
+import {createTask} from './addItems';
+import {removeElement} from './removeElements';
+
+// Charge assignmet containers from local storage
+let chargeAssigmentContainer = (assigmnentName) =>{
+
+  // Create and select elements 
+  let assigmentContainer = document.createElement('div'),
+      assigmentHeader = document.createElement('div'),
+      assigmentName = document.createElement('div'),
+      addTaskButton = document.createElement('div'),
+      taskButtonHover = document.createElement('div'),
+      assigmentTasks = document.createElement('div'),
+      container = document.querySelector(".overflowContent");
+
+  // Add classes to elements
+  assigmentContainer.classList.add("assignment");
+  assigmentHeader.classList.add("headerAssigment");
+  assigmentName.classList.add("assignmentName");
+  assigmentName.textContent = assigmnentName;
+  addTaskButton.classList.add("addToDo");
+  taskButtonHover.classList.add("displayHoverTask")
+  assigmentTasks.classList.add("toDo");
+
+  // Add button content 
+  addTaskButton.textContent = "+";
+  taskButtonHover.textContent = "Create new task";
+  addTaskButton.appendChild(taskButtonHover);
+  
+  // Add Assigment function onclick and id
+  addTaskButton.onclick = createTask.bind(this, assigmnentName);
+  assigmentTasks.id = assigmnentName;
+
+  // Add elements to Dom
+  assigmentHeader.appendChild(assigmentName);
+  assigmentHeader.appendChild(addTaskButton);
+  assigmentContainer.appendChild(assigmentHeader);
+  assigmentContainer.appendChild(assigmentTasks);  
+  container.appendChild(assigmentContainer);
+
+}
+
+// Charge taks from local storage
+let chargeTask = (assigmnent, taskText, Counter) =>{
+  // Create elements
+  let toDoContainer = document.querySelector("#taskGeneralContainer"),
+      checkboxContainer = document.createElement('div'),
+      checkboxText = document.createElement('div'),
+      checkBox =  document.createElement('div'), 
+      deletebox = document.createElement('div');
+
+  // add classes
+  checkboxContainer.classList.add("checkboxContainer");
+  checkBox.classList.add("checkBox"); 
+  deletebox.classList.add("deleteBox"); 
+
+  // add text
+  checkboxText.innerText += taskText; 
+  deletebox.innerText += "x"; 
+
+  // Add function that remove elements
+  deletebox.onclick = removeElement;
+
+ // fuction that add id and toggle fuction
+  chargeId(checkBox, assigmnent, Counter); 
+
+  // Append elements
+  checkboxContainer.appendChild(checkBox);
+  checkboxContainer.appendChild(checkboxText);
+  checkboxContainer.appendChild(deletebox);
+
+  // Add individual task to the parent with the ID
+  if (assigmnent == "taskGeneral"){
+    toDoContainer.appendChild(checkboxContainer);
+  } else {
+    document.querySelector("#"+assigmnent).appendChild(checkboxContainer);
+  }
+}
+
+export {
+  chargeAssigmentContainer,
+  chargeTask,
+  removeElement
+}
